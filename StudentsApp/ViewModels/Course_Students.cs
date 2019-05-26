6f2ae247bdab9fc79a68c553dcd73a9be228b6a5
Eduardo Antonio Fernandez Diaz <fernandez9000@gmail.com>
2019-05-26 01:38:40 -0500

    Course_Student ViewModel added and views modified

--- StudentsApp/ViewModels/Course_Students.cs
+++ StudentsApp/ViewModels/Course_Students.cs
@@ -2,8 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using StudentsApp.Models;
 
-namespace StudentsApp.Models
+namespace StudentsApp.ViewModels
 {
     public class Course_Students
     {
