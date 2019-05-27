5eec93f4affd6063ad1ae54b48767dc7ec72c82e
Eduardo Antonio Fernandez Diaz <fernandez9000@gmail.com>
2019-05-26 23:11:00 -0500

    Data annotations and Date Humanized

---
+++ StudentsApp/Helpers/HtmlHelpers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Humanizer;
+
+namespace StudentsApp.Helpers
+{
+    public static class HtmlHelpers
+    {
+        // We extend the HtmlHelper class and define a Trucate method
+        public static string Truncate(this HtmlHelper helper, string input, int length)
+        {
+            if (input.Length <= length)
+                return input;
+            else
+                return input.Substring(0,length) + "...";
+        }
+        public static string HumanDate(this HtmlHelper helper, DateTime input)
+        {
+            return input.Humanize();
+        }
+    }
+}
--- StudentsApp/Models/Student.cs
+++ StudentsApp/Models/Student.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,8 +9,11 @@ namespace StudentsApp.Models
     public class Student
     {
         public int StudentId { get; set; }
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
+        [Display(Name = "Date enrolled")]
         public DateTime EnrollmentDate { get; set; }
         // Every student will have a collection of Enrollments
         // Specifies One to Many relation to Enrollment model
